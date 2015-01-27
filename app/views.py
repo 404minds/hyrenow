5a7cbd9490655662e3374d5b57e1b879b1afcb74
Kshitij Mittal <kshitij.mittal01@gmail.com>
2015-01-28 02:54:28 +0530

    Kshitij: Ajax function added to utils.js class, sending data from signup frontend to backend

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect,jsonify
+from flask import render_template, flash, redirect,jsonify,request
 from app import app
 from .forms import LoginForm
 @app.route('/')
@@ -7,3 +7,14 @@ def index():
 	return jsonify(username="Kshitij2",
                    email="kshitij.mittal01@gmail.com",
                    id="7")
+
+
+@app.route('/signup', methods=['POST'])
+def login():
+	name=request.json['name']
+	email=request.json['email']
+	password=request.json['password']
+	# print username
+	return jsonify(name=name,
+                   email=email,
+                   password=password)
